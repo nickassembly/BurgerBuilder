4e3aa649ee20d35b41408ad70d3ed76d3755050d
Nick Guerra <nickg@maliciousgames.com>
2020-10-25 07:52:15 -0500

    added toolbar

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,10 +2,11 @@ import React from 'react';
 
 import Auxillary from '../../hoc/Auxillary';
 import classes from './Layout.module.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
 
 const layout = (props) => (
   <Auxillary>
-    <div>Toolbar, SideDrawer, Backdrop</div>
+    <Toolbar />
     <main className={classes.Content}>{props.children}</main>
   </Auxillary>
 );
