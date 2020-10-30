910d56e2acc86c8989714d0828d932648c78dfb9
Nick Guerra <nickg@maliciousgames.com>
2020-10-30 07:42:52 -0500

    change folder structure

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,21 +1,33 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import classes from './Modal.module.css';
-import Auxillary from '../../../hoc/Auxillary';
+import Auxillary from '../../../hoc/Auxillary/Auxillary';
 import Backdrop from '../Backdrop/Backdrop';
 
-const modal = (props) => (
-  <Auxillary>
-    <Backdrop show={props.show} clicked={props.modalClosed} />
-    <div
-      className={classes.Modal}
-      style={{
-        transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-        opacity: props.show ? '1' : '0',
-      }}>
-      {props.children}
-    </div>
-  </Auxillary>
-);
+class Modal extends Component {
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextProps.show !== this.props.show;
+  }
 
-export default modal;
+  componentWillUpdate() {
+    console.log('[Modal] will update');
+  }
+
+  render() {
+    return (
+      <Auxillary>
+        <Backdrop show={this.props.show} clicked={this.props.modalClosed} />
+        <div
+          className={classes.Modal}
+          style={{
+            transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+            opacity: this.props.show ? '1' : '0',
+          }}>
+          {this.props.children}
+        </div>
+      </Auxillary>
+    );
+  }
+}
+
+export default Modal;
