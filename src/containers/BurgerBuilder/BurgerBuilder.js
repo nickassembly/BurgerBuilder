33d3e348654b2759baa5236fdb77cc14eaa1e6da
nickguerra <nickguerra@gmail.com>
2020-11-25 05:51:03 -0600

    added routing to project

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -28,6 +28,7 @@ class BurgerBuilder extends Component {
   };
 
   componentDidMount() {
+    console.log(this.props);
   axios.get('https://react-my-burger-b355c.firebaseio.com/ingredients.json')
        .then(response => {
          this.setState({ingredients: response.data});
@@ -89,29 +90,30 @@ class BurgerBuilder extends Component {
 
   purchaseContinueHandler = () => {
     // alert('You continue');
-    this.setState({loading: true});
+    // this.setState({loading: true});
 
-    const order = {
-      ingredients: this.state.ingredients,
-      price: this.state.totalPrice,
-      customer: {
-        name: 'Nick Keller',
-        address: {
-          street: 'Teststreet 1',
-          zipCode: '22344',
-          country: 'USA'
-        },
-        email: 'test@test.com'
-      },
-      deliveryMethod: 'fastest'
-    }
-    axios.post('/orders.json', order)
-    .then(response => {
-      this.setState({loading: false, purchasing: false});
-    })
-    .catch(error => {
-      this.setState({loading: false, purchasing: false});
-    });
+    // const order = {
+    //   ingredients: this.state.ingredients,
+    //   price: this.state.totalPrice,
+    //   customer: {
+    //     name: 'Nick Keller',
+    //     address: {
+    //       street: 'Teststreet 1',
+    //       zipCode: '22344',
+    //       country: 'USA'
+    //     },
+    //     email: 'test@test.com'
+    //   },
+    //   deliveryMethod: 'fastest'
+    // }
+    // axios.post('/orders.json', order)
+    // .then(response => {
+    //   this.setState({loading: false, purchasing: false});
+    // })
+    // .catch(error => {
+    //   this.setState({loading: false, purchasing: false});
+    // });
+    this.props.history.push('/checkout');
   };
 
   render() {
