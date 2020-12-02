18344f246bf2553fadc1d50ae16815e91af68621
nickguerra <nickguerra@gmail.com>
2020-12-01 18:15:18 -0600

    added validation styles

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -3,22 +3,28 @@ import classes from './Input.module.css';
 
 const input = (props) => {
     let inputElement = null;
+    const inputClasses = [classes.InputElement];
+
+    if(props.invalid && props.shouldValidate && props.touched) {
+        inputClasses.push(classes.Invalid);
+    }
+
     switch(props.elementType) {
         case('input'):
-        inputElement= <input className={classes.InputElement} 
+        inputElement= <input className={inputClasses.join(' ')} 
                           onChange={props.changed}                 
                           {...props.elementConfig} 
                           value={props.value}/>;
         break;
         case ('textarea'):
-           inputElement = <textarea className={classes.InputElement}  
+           inputElement = <textarea className={inputClasses.join(' ')}  
                            onChange={props.changed}
                            {...props.elementConfig}
                             value={props.value}/>;                       
            break;
            case ('select'):
             inputElement = (<select
-                           className={classes.InputElement} 
+                           className={inputClasses.join(' ')} 
                            onChange={props.changed}
                            value={props.value}>
                             {props.elementConfig.options.map(option => (
@@ -29,7 +35,7 @@ const input = (props) => {
                             </select>);                       
             break;
            default: 
-           inputElement = <input className={classes.InputElement} 
+           inputElement = <input className={inputClasses.join(' ')} 
                            onChange={props.changed}
                            {...props.elementConfig}
                             value={props.value}/>;
