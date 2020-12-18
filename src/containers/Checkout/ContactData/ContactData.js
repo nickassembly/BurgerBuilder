2c883f740430378800487734cb194f0d0fe7e110
nickguerra <nickguerra@gmail.com>
2020-12-18 07:10:41 -0600

    added new action types

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -5,6 +5,8 @@ import Spinner from '../../../components/UI/Spinner/Spinner';
 import classes from './ContactData.module.css';
 import axios from '../../../axios-orders';
 import Input from '../../../components/UI/Input/Input';
+import withErrorHandler from '../../../hoc/withErrorHandler/withErrorHandler';
+import * as actions from '../../../store/actions/index';
 
 class ContactData extends Component {
 	state = {
@@ -95,7 +97,7 @@ class ContactData extends Component {
 
 	orderHandler = (event) => {
 		event.preventDefault();
-		this.setState({ loading: true });
+
 		const formData = {};
 		for (let formElementIdentifier in this.state.orderForm) {
 			formData[formElementIdentifier] = this.state.orderForm[
@@ -107,15 +109,8 @@ class ContactData extends Component {
 			price: this.props.price,
 			orderData: formData,
 		};
-		axios
-			.post('/orders.json', order)
-			.then((response) => {
-				this.setState({ loading: false });
-				this.props.history.push('/');
-			})
-			.catch((error) => {
-				this.setState({ loading: false });
-			});
+
+		this.props.onOrderBurger(order);
 	};
 
 	checkValidity(value, rules) {
@@ -179,7 +174,7 @@ class ContactData extends Component {
 		let form = (
 			<form onSubmit={this.orderHandler}>
 				{formElementsArray.map((formElement) => (
-        <Input
+					<Input
 						key={formElement.id}
 						elementType={formElement.config.elementType}
 						elementConfig={formElement.config.elementConfig}
@@ -216,4 +211,9 @@ const mapStateToProps = (state) => {
 	};
 };
 
-export default connect(mapStateToProps)(ContactData);
+const mapDispatchToProps = (dispatch) => ({
+	onOrderBurger: (orderData) =>
+		dispatch(actions.purchaseBurgerStart(orderData)),
+});
+
+export default connect(mapStateToProps)(withErrorHandler(ContactData, axios));
