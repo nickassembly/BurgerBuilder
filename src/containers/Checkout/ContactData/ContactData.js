87f2eca0855cecebb5e8592967d135b6c6ad6d0e
nickguerra <nickguerra@gmail.com>
2020-12-19 06:30:41 -0600

    fixed errors in burger order screen

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -92,7 +92,6 @@ class ContactData extends Component {
 			},
 		},
 		formIsValid: false,
-		loading: false,
 	};
 
 	orderHandler = (event) => {
@@ -192,7 +191,7 @@ class ContactData extends Component {
 				</Button>
 			</form>
 		);
-		if (this.state.loading) {
+		if (this.props.loading) {
 			form = <Spinner />;
 		}
 		return (
@@ -208,12 +207,18 @@ const mapStateToProps = (state) => {
 	return {
 		ings: state.ingredients,
 		price: state.totalPrice,
+		loading: state.loading,
 	};
 };
 
-const mapDispatchToProps = (dispatch) => ({
-	onOrderBurger: (orderData) =>
-		dispatch(actions.purchaseBurgerStart(orderData)),
-});
+const mapDispatchToProps = (dispatch) => {
+	return {
+		onOrderBurger: (orderData) =>
+			dispatch(actions.purchaseBurger(orderData)),
+	};
+};
 
-export default connect(mapStateToProps)(withErrorHandler(ContactData, axios));
+export default connect(
+	mapStateToProps,
+	mapDispatchToProps
+)(withErrorHandler(ContactData, axios));
