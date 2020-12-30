ce85b08a8c16cdd43220d93fa3bd7e7bd62eddc6
nickguerra <nickguerra@gmail.com>
2020-12-30 06:00:00 -0600

    added auto logout

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -36,6 +36,7 @@ class BurgerBuilder extends Component {
 		if (this.props.isAuthenticated) {
 			this.setState({ purchasing: true });
 		} else {
+			this.props.onSetAuthRedirectPath('/checkout');
 			this.props.history.push('/auth');
 		}
 	};
@@ -120,6 +121,8 @@ const mapDispatchToProps = (dispatch) => {
 			dispatch(actions.removeIngredient(ingName)),
 		onInitIngredients: () => dispatch(actions.initIngredients()),
 		onInitPurchase: () => dispatch(actions.purchaseInit()),
+		onSetAuthRedirectPath: (path) =>
+			dispatch(actions.setAuthRedirectPath(path)),
 	};
 };
 
