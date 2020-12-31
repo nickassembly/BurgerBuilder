ec2f73d58497017adfbef2b75af525a993ae09cd
nickguerra <nickguerra@gmail.com>
2020-12-31 05:51:54 -0600

    added guard routes

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -91,14 +91,14 @@ export const authCheckState = () => {
 			const expirationDate = new Date(
 				localStorage.getItem('expirationDate')
 			);
-			if (expirationDate > new Date()) {
+			if (expirationDate <= new Date()) {
 				dispatch(logout());
 			} else {
 				const userId = localStorage.getItem('userId');
 				dispatch(authSuccess(token, userId));
 				dispatch(
 					checkAuthTimeout(
-						expirationDate.getSeconds() - new Date().getSeconds()
+						(expirationDate.getTime() - new Date().getTime()) / 1000
 					)
 				);
 			}
